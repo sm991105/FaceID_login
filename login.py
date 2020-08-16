604bc3c858db7226ff408d48b253c58c0a446712
Suemin Cho <sm991105@gmail.com>
2020-08-16 23:15:05 +0900

    almost there

--- login.py
+++ login.py
@@ -85,87 +85,98 @@ def member_login():
                 # 2차 인증 - faceid
                 encoding_file = "encodings.pickle"
 
-                def detectAndDisplay(frame):
-                    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    boxes = face_recognition.face_locations(rgb, model="HOG")
-                    encodings = face_recognition.face_encodings(rgb, boxes)
-                    names = []
-                    for encoding in encodings:
-                        matches = face_recognition.compare_faces(
-                            data["encodings"], encoding, tolerance=0.4
-                        )
-                        name = "unknown"
+                # def detectAndDisplay(frame):
 
-                        if True in matches:
-                            matchedIndxs = []
-                            for (i, b) in enumerate(matches):
-                                if b == True:
-                                    matchedIndxs.append(i)
-
-                            counts = {}
-                            for items in matchedIndxs:
-                                name = data["names"][items]
-                                counts[name] = 0
-                            for items in matchedIndxs:
-                                counts[data["names"][items]] = (
-                                    counts.get(data["names"][items]) + 1
-                                )
-                            name = max(counts, key=counts.get)
-                            print()
-                            names.append(name)
-                        else:
-                            return render_template("./login.html")
-
-                    for ((top, right, bottom, left), name) in zip(boxes, names):
-                        y = top - 15
-                        color = (255, 255, 0)
-                        line = 1
-                        if name == "unknown":
-                            color = (255, 255, 255)
-                            line = 1
-                            name = ""
-                        cv2.rectangle(frame, (left, top), (right, bottom), color, line)
-                        cv2.putText(
-                            frame,
-                            name,
-                            (left, y),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            0.75,
-                            color,
-                            line,
-                        )
-
-                    cv2.imshow("Recognition", frame)
-
-                    if counts[pdata["name"]] > 6:
-                        print("it's" + pdata["name"] + "!")
-                        global cnt
-                        cnt += 1
-                        print(cnt)
-                    else:
-                        return render_template("./login.html")
-
+                # Starts here
                 data = pickle.loads(open(encoding_file, "rb").read())
 
                 cap = cv2.VideoCapture(0)
-
                 while True:
                     ret, frame = cap.read()
                     if frame is None:
                         print("No more captured frames!")
                         break
                     flipped = cv2.flip(frame, 1)
-                    detectAndDisplay(flipped)
+                    # detectAndDisplay(flipped)
+                    rgb = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
+                    boxes = face_recognition.face_locations(rgb, model="HOG")
+                    encodings = face_recognition.face_encodings(rgb, boxes)
+                    names = []
+                    if encodings:
+                        print("encodings exist")
+                        for encoding in encodings:
+                            matches = face_recognition.compare_faces(
+                                data["encodings"], encoding, tolerance=0.4
+                            )
+                            name = "unknown"
+
+                            if True in matches:
+                                matchedIndxs = []
+                                for (i, b) in enumerate(matches):
+                                    if b == True:
+                                        matchedIndxs.append(i)
+
+                                counts = {}
+                                for items in matchedIndxs:
+                                    name = data["names"][items]
+                                    counts[name] = 0
+                                for items in matchedIndxs:
+                                    counts[data["names"][items]] = (
+                                        counts.get(data["names"][items]) + 1
+                                    )
+                                name = max(counts, key=counts.get)
+                                print()
+                                names.append(name)
+                            else:
+                                flash("얼굴을 인식시켜주세요1")
+
+                        for ((top, right, bottom, left), name) in zip(boxes, names):
+                            y = top - 15
+                            color = (255, 255, 0)
+                            line = 1
+                            if name == "unknown":
+                                color = (255, 255, 255)
+                                line = 1
+                                name = ""
+                            cv2.rectangle(
+                                flipped, (left, top), (right, bottom), color, line
+                            )
+                            cv2.putText(
+                                flipped,
+                                name,
+                                (left, y),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                0.75,
+                                color,
+                                line,
+                            )
+
+                        cv2.imshow("Recognition", flipped)
+
+                        if counts[pdata["name"]] > 6:
+                            print("it's " + pdata["name"] + "!")
+                            global cnt
+                            cnt += 1
+                            print(cnt)
+                        else:
+                            return render_template("./login.html")
+                    else:
+                        print("encoding doesn't exist")
+                        flash("카메라에 얼굴을 인식시켜주세요.")
+                        return render_template("./login.html")
+
                     if cv2.waitKey(1) & 0xFF == ord("q"):
                         break
 
-                    if cnt % 5 == 0:
+                    if cnt == 5:
+                        print("by 5")
                         cap.release()
                         cv2.destroyAllWindows()
                         return render_template("./success.html")
 
                 cap.release()
                 cv2.destroyAllWindows()
+                cnt = 0
                 # 여기까지
                 # return render_template("./success.html")
             else:
